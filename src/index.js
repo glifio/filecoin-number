5bd5d9bc49ec9ce75aaf16f4503b07a01ff17618
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2020-01-21 11:43:20 -0500

    Update bignumber config, bump version

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const BigNumber = require('bignumber.js')
 
+// not sure how we want to configure rounding for this
+BigNumber.set({ ROUNDING_MODE: BigNumber.ROUND_HALF_DOWN })
+BigNumber.config({ EXPONENTIAL_AT: [-19, 20] })
+
 // stores filecoin numbers in denominations of Fil, not AttoFil
 class FilecoinNumber extends BigNumber {
   constructor(amount, denom) {
