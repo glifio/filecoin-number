3fe52a9b7a667c09630ead10f0f5d889c3811ea3
listenaddress <tbobrien612@gmail.com>
2020-04-11 10:17:52 -0700

    Migrate to Jest

--- test/number.spec.js
+++ test/number.spec.js
@@ -1,46 +1,30 @@
-const assert = require('assert')
-
 const { FilecoinNumber, BigNumber } = require('../')
 
 describe('FilecoinNumber', () => {
   it('should return instances of BigNumber', () => {
     const filecoinNum = new FilecoinNumber('5000000000000000', 'attofil')
-    assert.equal(BigNumber.isBigNumber(filecoinNum), true)
+    expect(BigNumber.isBigNumber(filecoinNum)).toBe(true)
   })
 
   it('converts the same number into both fil and attofil denominations', () => {
     const attoFilecoinNum = new FilecoinNumber('5000000000000000', 'attofil')
-    assert.equal(attoFilecoinNum.toFil(), '0.005')
-    assert.equal(attoFilecoinNum.toAttoFil(), '5000000000000000')
+    expect(attoFilecoinNum.toFil()).toEqual('0.005')
+    expect(attoFilecoinNum.toAttoFil()).toEqual('5000000000000000')
 
     const filecoinNum = new FilecoinNumber('0.005', 'fil')
-    assert.equal(filecoinNum.toFil(), '0.005')
-    assert.equal(filecoinNum.toAttoFil(), '5000000000000000')
+    expect(filecoinNum.toFil()).toEqual('0.005')
+    expect(filecoinNum.toAttoFil()).toEqual('5000000000000000')
   })
 
   it('throws error if no denom is specified in constructor', () => {
-    // assert.throws not working for some reason?
-    try {
-      new FilecoinNumber('0.005')
-    } catch (err) {
-      assert.equal(err instanceof Error, true)
-      assert.equal(
-        err.message,
-        'No Filecoin denomination passed in constructor.',
-      )
-    }
+    expect(() => new FilecoinNumber('0.005')).toThrow(
+      'No Filecoin denomination passed in constructor.',
+    )
   })
 
-  it('throws error if no denom is specified in constructor', () => {
-    // assert.throws not working for some reason?
-    try {
-      new FilecoinNumber('0.005', 'ottofil')
-    } catch (err) {
-      assert.equal(err instanceof Error, true)
-      assert.equal(
-        err.message,
-        'Unsupported denomination passed in constructor.',
-      )
-    }
+  it('throws error if invalid denom is specified in constructor', () => {
+    expect(() => new FilecoinNumber('0.005', 'fakefil')).toThrow(
+      'Unsupported denomination passed in constructor.',
+    )
   })
 })
