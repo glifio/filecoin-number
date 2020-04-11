811d9749f45fcf5c7532f301270b01ef77cef913
listenaddress <tbobrien612@gmail.com>
2020-04-11 13:35:13 -0700

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,7 @@
 module.exports = {
   collectCoverageFrom: ['**/*.{js}', '!**/node_modules/**'],
   testPathIgnorePatterns: ['/node_modules/', '/module/', '/lib/', '/dist/'],
-  transformIgnorePatterns: [
-    '/node_modules/',
-    '^.+\\.module\\.(css|sass|scss)$',
-  ],
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
-  setupFilesAfterEnv: ['./jest.setup.js'],
 }
